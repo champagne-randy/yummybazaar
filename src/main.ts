8984638df372706df498e9eda0412704887a56c9
Randy Champagne <r-champagne@neiu.edu>
2017-08-05 13:46:07 -0400

    added logic to switch productionMode on

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// NG core modules
+import {
+	enableProdMode
+} 							from '@angular/core';
+import { 
+	platformBrowserDynamic 
+} 							from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+// Project Modules
+import { 
+	AppModule 
+} 							from './app/app.module';
 
+// Project Utils
+import { 
+	environment 
+} 							from '../environments/environment';
+
+
+// set App in prod mode depending on env
+if (environment.production)
+	enableProdMode();
+
+// bootstrap AppModule
 platformBrowserDynamic().bootstrapModule(AppModule);
 
 
